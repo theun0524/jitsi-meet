b5dfe2377eb4b20a82e9c57fa150882c0d3f10fd
오수경 <ohsk23@postech.ac.kr>
2020-07-21 02:08:46 +0900

    Change api url config

--- react/api/AuthApi.js
+++ react/api/AuthApi.js
@@ -1,36 +1,34 @@
 import axios from "axios";
 import { API_URL } from "../config";
 
-const apiRoot = process.env.API_URL;
-
 export function fetchCurrentUser() {
-  return axios.get(`${apiRoot}/current-user`, { withCredentials: true });
+  return axios.get(`${API_URL}/current-user`, { withCredentials: true });
 }
 
 export function login(form) {
-  return axios.post(`${apiRoot}/login`, form);
+  return axios.post(`${API_URL}/login`, form);
 }
 
 export function logout() {
-  return axios.get(`${apiRoot}/logout`);
+  return axios.get(`${API_URL}/logout`);
 }
 
 export function signup(form) {
-  return axios.post(`${apiRoot}/signup`, form);
+  return axios.post(`${API_URL}/signup`, form);
 }
 
 export function passwordReset(form) {
-  return axios.post(`${apiRoot}/password-reset`, form);
+  return axios.post(`${API_URL}/password-reset`, form);
 }
 
 export function updateAccount(form) {
-  return axios.patch(`${apiRoot}/account`, form);
+  return axios.patch(`${API_URL}/account`, form);
 }
 
 export function passwordResetConfirm(form) {
-  return axios.post(`${apiRoot}/password-reset-confirm`, form);
+  return axios.post(`${API_URL}/password-reset-confirm`, form);
 }
 
 export function updatePassword(form) {
-  return axios.patch(`${apiRoot}/password-reset-confirm`, form);
+  return axios.patch(`${API_URL}/password-reset-confirm`, form);
 }
