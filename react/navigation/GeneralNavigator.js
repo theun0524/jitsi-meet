919455d52584ac36e9b704bef4eefc7ceb89cfb8
ohsk23 <46521871+ohsk23@users.noreply.github.com>
2020-07-24 21:24:53 +0900

    Add setting screen (#12)
    * Change api url config
    
    * Add ios status bar height
    
    * Add logout button at setting
    
    * Add scrollview for android
    
    * Add locale for registerscreen
    
    * Fix login logics
    
    * Modify homescreen logic

--- react/navigation/GeneralNavigator.js
+++ react/navigation/GeneralNavigator.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import LoginScreen from "../screens/LoginScreen/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen/RegisterScreen";
 import PasswordResetScreen from "../screens/PasswordResetScreen/PasswordResetScreen";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import AsyncStorage from "@react-native-community/async-storage";
 import { JWT_TOKEN } from "../config";
 import JwtDecode from "jwt-decode";
+import { setScreen } from "../redux/screen/screen";
 
-const GeneralNavigator = ({ appProps, Home }) => {
+const GeneralNavigator = ({ Home }) => {
+  const dispatch = useDispatch();
   const currScreen = useSelector((store) => store.screen.currScreen);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
   useEffect(() => {
     console.log(currScreen);
   }, [currScreen]);
@@ -19,15 +20,19 @@ const GeneralNavigator = ({ appProps, Home }) => {
     if (token) {
       const { context } = JwtDecode(token);
       if (context.user) {
-        setIsAuthenticated(true);
+        dispatch(setScreen("Home"));
+      } else {
+        dispatch(setScreen("Login"));
       }
+    } else {
+      dispatch(setScreen("Login"));
     }
   };
   useEffect(() => {
     checkAuthorizedUser();
-  });
+  }, []);
 
-  return isAuthenticated && currScreen === "Home" ? (
+  return currScreen === "Home" ? (
     Home
   ) : currScreen === "Register" ? (
     <RegisterScreen />
