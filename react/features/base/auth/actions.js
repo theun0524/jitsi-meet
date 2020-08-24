52e9ce90258dcdb1b216fbfa9274560c5a4e0be3
theun0524 <theun@postech.ac.kr>
2020-08-24 14:45:15 +0900

    fix eslint

--- react/features/base/auth/actions.js
+++ react/features/base/auth/actions.js
@@ -1,11 +1,13 @@
 /* global process */
 // import axios from 'axios';
 
-import jwtDecode from 'jwt-decode';
-// import logger from './logger';
-import { SET_CURRENT_USER } from './actionTypes';
 import { jitsiLocalStorage } from '@jitsi/js-utils';
+import jwtDecode from 'jwt-decode';
+
+// import logger from './logger';
 import { setJWT } from '../jwt';
+
+import { SET_CURRENT_USER } from './actionTypes';
 
 // const AUTH_API_BASE = process.env.REACT_APP_AUTH_API_BASE;
 const AUTH_JWT_TOKEN = process.env.JWT_APP_ID;
@@ -26,8 +28,10 @@ export function loadCurrentUser() {
         // }
         try {
             const token = jitsiLocalStorage.getItem(AUTH_JWT_TOKEN);
+
             if (token) {
                 const { exp } = jwtDecode(token);
+
                 // check expire of jwt token
                 if (Date.now() < exp * 1000) {
                     dispatch(setJWT(token));
@@ -35,7 +39,7 @@ export function loadCurrentUser() {
                     jitsiLocalStorage.removeItem(AUTH_JWT_TOKEN);
                 }
             }
-        } catch(e) {
+        } catch (e) {
             console.error('loadCurrentUser is failed:', e.message);
         }
     };
