313a08c3589529885a499562f07d44af46671244
은태환 <theun@euntaehwan-ui-MacBookPro.local>
2020-12-11 12:57:13 +0900

    improve background effect

--- react/features/stream-effects/background/VmeetingStreamBackgroundEffect.js
+++ react/features/stream-effects/background/VmeetingStreamBackgroundEffect.js
@@ -1,6 +1,7 @@
 // @flow
 
 import * as bodyPix from '@tensorflow-models/body-pix';
+import { flip } from 'lodash';
 
 import {
     CLEAR_INTERVAL,
@@ -70,6 +71,7 @@ export default class VmeetingStreamBackgroundEffect {
             segmentationThreshold: 0.7 // represents probability that a pixel belongs to a person
         });
         this._videoInProgress = false;
+
         this.drawBody();
     }
 
@@ -155,19 +157,18 @@ export default class VmeetingStreamBackgroundEffect {
         if (!this._segmentationData) return;
 
         const { width, height } = this._videoElement;
-
-        this._ctx.drawImage(this._videoElement, 0, 0, width, height);
-        const mixData = this._ctx.getImageData(0, 0, width, height);
-        const pixel = mixData.data;
-
-        const foregroundColor = { r: 0, g: 0, b: 0, a: 255 };
-        const backgroundColor = { r: 0, g: 0, b: 0, a: 0 };
+        const foregroundColor = { r: 0, g: 0, b: 0, a: 0 };
+        const backgroundColor = { r: 0, g: 0, b: 0, a: 255 };
         const coloredPartImage = bodyPix.toMask(this._segmentationData, foregroundColor, backgroundColor);
         const opacity = 1;
         const flipHorizontal = false;
         const maskBlurAmount = 3;
-        bodyPix.drawMask(this._canvas, this._backgroundElement, coloredPartImage, opacity, maskBlurAmount, flipHorizontal);
-        // this._ctx.drawImage(this._backgroundElement, 0, 0, width, height);        const back = this._ctx.getImageData(0, 0, width, height).data;
+        bodyPix.drawMask(this._canvas, this._videoElement, coloredPartImage, opacity, maskBlurAmount, flipHorizontal);
+        const mixData = this._ctx.getImageData(0, 0, width, height);
+        const pixel = mixData.data;
+
+        this._ctx.drawImage(this._backgroundElement, 0, 0, width, height);
+        const back = this._ctx.getImageData(0, 0, width, height).data;
         for (let p = 0; p < pixel.length; p += 4) {
             if (this._segmentationData.data[p/4] == 0) {
                 pixel[p] = back[p];
