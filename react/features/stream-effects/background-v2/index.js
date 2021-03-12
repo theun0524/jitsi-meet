cbfd38aaf1636c002b5d993fbc0566b49e8aead6
SangwooRyu <mrryu006@naver.com>
2021-03-12 17:24:07 +0900

    background-v2 not using TFLite

--- react/features/stream-effects/background-v2/index.js
+++ react/features/stream-effects/background-v2/index.js
@@ -1,7 +1,26 @@
 // @flow
 
-import useTFLite from './core/hooks/useTFLite'
+import * as wasmCheck from 'wasm-check';
+
 import VmeetingStreamBackgroundEffectV2 from './VmeetingStreamBackgroundEffectV2';
+import createTFLiteModule from '../background-v2/tflite/tflite';
+import createTFLiteSIMDModule from '../background-v2/tflite/tflite-simd';
+
+const models = {
+    'model96': 'libs/bg_segmentation_lite.tflite',
+    'model144': 'libs/bg_segmentation.tflite'
+};
+
+const segmentationDimensions = {
+    'model96': {
+        'height': 96,
+        'width': 160
+    },
+    'model144': {
+        'height': 144,
+        'width': 256
+    }
+};
 
 /**
  * Creates a new instance of VmeetingStreamBackgroundEffect. This loads the bodyPix model that is used to
@@ -13,10 +32,31 @@ export async function createBackgroundEffectV2(backgroundImageUrl) {
     if (!MediaStreamTrack.prototype.getSettings && !MediaStreamTrack.prototype.getConstraints) {
         throw new Error('VmeetingStreamBackgroundEffectV2 not supported!');
     }
-    let useLite = false;
 
-    var tflite = await useTFLite(useLite);
-    console.log("tflite: ", tflite);
+    let tflite;
 
-    return new VmeetingStreamBackgroundEffectV2(tflite, backgroundImageUrl, useLite);
+    if (wasmCheck.feature.simd) {
+        tflite = await createTFLiteSIMDModule();
+    } else {
+        tflite = await createTFLiteModule();
+    }
+
+    const modelBufferOffset = tflite._getModelBufferMemoryOffset();
+    const modelResponse = await fetch(
+        wasmCheck.feature.simd ? models.model144 : models.model96
+    );
+
+    if (!modelResponse.ok) {
+        throw new Error('Failed to download tflite model!');
+    }
+
+    const model = await modelResponse.arrayBuffer();
+
+    tflite.HEAPU8.set(new Uint8Array(model), modelBufferOffset);
+
+    tflite._loadModel(model.byteLength);
+
+    //const options = wasmCheck.feature.simd ? segmentationDimensions.model144 : segmentationDimensions.model96;
+
+    return new VmeetingStreamBackgroundEffectV2(tflite, backgroundImageUrl, !wasmCheck.feature.simd);
 }
