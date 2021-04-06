29ca5fbb1664cc1bbe4531465c52463b7b4e1561
sajan-dpnm <thesajan@postech.ac.kr>
2021-04-06 01:00:33 +0000

    Removed redundant event listener for next result while scrolling chat search results

--- react/features/chat/components/web/Chat.js
+++ react/features/chat/components/web/Chat.js
@@ -444,12 +444,7 @@ class Chat extends AbstractChat<Props> {
                 animation: false });
         }
 
-        // navigate to next result when there are search results
-        else if(count > 0) {
-            document.addEventListener('keyup', this._nextResult);
-        }
 
-        // remove event listener on key press escape
         
     }
 
